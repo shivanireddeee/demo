8dbb5e9f6ecf01724917ee04a5d4646d9104c20d
shivanireddeee <128642175+shivanireddeee@users.noreply.github.com>
2024-03-13 16:07:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, jsonify
+from flask import Flask, render_template, request, redirect
 from supabase import create_client
 import os
 from dotenv import load_dotenv
@@ -15,30 +15,52 @@ supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-@app.route('/supabase_data')
-def supabase_data():
     # Fetch data from the 'test' table
     response = supabase.table('test').select('*').execute()
     # Use the 'data' attribute to access the data returned by the query
-    data = response['data']
-    return jsonify(data)
+    data = response.data
+    return render_template('index.html', data=data)
+
+
 
 @app.route('/submit', methods=['POST'])
 def submit_form():
-    data = request.json
-    id = data['id']
-    name = data['name']
+    id = request.form['id']
+    name = request.form['name']
     # Insert data into 'test' table
     response = supabase.table('test').insert({'id': id, 'name': name}).execute()
-    return '', 204
+    return redirect('/')
+
 
 @app.route('/delete/<int:id>', methods=['DELETE'])
 def delete_record(id):
     # Delete record from 'test' table
     response = supabase.table('test').delete().eq('id', id).execute()
-    return '', 204
+    return redirect('/')
+
+# @app.route('/update/<int:id>', methods=['UPDATE'])
+# def update_record(id):
+#     response = supabase.table('test').update({'id': 'id'}).eq('id', id).execute()
+#     return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000, debug=True)
+
+
+
